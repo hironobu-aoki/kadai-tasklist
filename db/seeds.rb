15c2404d20a50998433f051a64c1c96572428e8f
Hironobu Aoki <aoki0702@gmail.com>
2017-09-27 05:23:17 +0000

    DRY

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+Task.create(content: 'test content 1')
+Task.create(content: 'test content 2')
+Task.create(content: 'test content 3')
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
